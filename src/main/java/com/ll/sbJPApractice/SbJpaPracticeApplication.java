76cab72ac6ed6ce21d4f29fbaae9d246050d9ce2
seo-sang-ho <rocter2049@naver.com>
2023-12-02 21:55:52 +0900

    init

---
+++ src/main/java/com/ll/sbJPApractice/SbJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sbJPApractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJpaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJpaPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/sbJPApractice/SbJpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbJPApractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
